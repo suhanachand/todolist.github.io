7d76074173e699d0f87cd06ce3f9fb9721ffc199
Suhana Chand <suhanachand03@gmail.com>
2025-10-22 19:27:47 -0700

    Delete public directory

--- public/js/pomodoro.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.render("pomodoro", { user: req.session.user || null });
-});
-
-export default router;
